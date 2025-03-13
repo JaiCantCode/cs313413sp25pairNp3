dd5cbb7b13f5501921619311e943deca0741cb31
Jai <jaifischer@gmail.com>
2025-03-13 16:05:30 -0500

    did some cool things

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -17,8 +17,8 @@ public class Draw implements Visitor<Void> {
     private final Paint paint;
 
     public Draw(final Canvas canvas, final Paint paint) {
-        this.canvas = null; // FIXME
-        this.paint = null; // FIXME
+        this.canvas = canvas; // FIXME
+        this.paint = paint; // FIXME
         paint.setStyle(Style.STROKE);
     }
 
@@ -30,37 +30,46 @@ public class Draw implements Visitor<Void> {
 
     @Override
     public Void onStrokeColor(final StrokeColor c) {
-
+        var color = paint.getColor();
+        paint.setColor(c.getColor());
+        c.getShape().accept(this);
+        paint.setColor(color);
         return null;
     }
 
     @Override
     public Void onFill(final Fill f) {
-
+        f.getShape().accept(this);
         return null;
     }
 
     @Override
     public Void onGroup(final Group g) {
-
+        for(Shape shape : g.getShapes()) {
+            shape.accept(this);
+        }
         return null;
     }
 
     @Override
     public Void onLocation(final Location l) {
-
+        canvas.save();
+        canvas.translate(l.getX(), l.getY());
+        l.getShape().accept(this);
+        canvas.translate(-l.getX(), -l.getY());
+        canvas.restore();
         return null;
     }
 
     @Override
     public Void onRectangle(final Rectangle r) {
-
+        canvas.drawRect(0,0,r.getWidth(),r.getHeight(),paint);
         return null;
     }
 
     @Override
     public Void onOutline(Outline o) {
-
+        o.getShape().accept(this);
         return null;
     }
 
--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
@@ -17,38 +17,58 @@ public class BoundingBox implements Visitor<Location> {
 
     @Override
     public Location onFill(final Fill f) {
-        return null;
+        return f.shape.accept(this);
     }
 
     @Override
     public Location onGroup(final Group g) {
+        int x, y, width, height;
+        x = y = Integer.MAX_VALUE;
+        width = height = 0;
 
-        return null;
+        for(Shape s : g.shapes) {
+            Location l = s.accept(this);
+            var r = (Rectangle) l.shape;
+            if(l.x < x) x = l.x;
+            if(l.y < y) y = l.y;
+            if(x + width < l.x + r.width) width = (l.x + r.width) - x;
+            if(y + height < l.y + r.height) height = (l.y + r.height) - y;
+        }
+        return new Location(x,y,new Rectangle(width, height));
     }
 
     @Override
     public Location onLocation(final Location l) {
-
-        return null;
+        Location temp = l.shape.accept(this);
+        return new Location(l.x + temp.x, l.y+ temp.y, temp.shape);
     }
 
     @Override
     public Location onRectangle(final Rectangle r) {
-        return null;
+        return new Location(0,0,new Rectangle(r.width, r.height));
     }
 
     @Override
     public Location onStrokeColor(final StrokeColor c) {
-        return null;
+        return c.shape.accept(this);
     }
 
     @Override
     public Location onOutline(final Outline o) {
-        return null;
+        return o.shape.accept(this);
     }
 
     @Override
     public Location onPolygon(final Polygon s) {
-        return null;
+        int x, y, width, height;
+        x = y = Integer.MAX_VALUE;
+        width = height = 0;
+        for(Point p : s.getPoints()) {
+            if(p.x < x) x = p.x;
+            if(p.y < y) y = p.y;
+            if(p.x > width) width = p.x;
+            if(p.y > height) height = p.y;
+        }
+        return new Location(x,y,new Rectangle(width-x, height-y));
     }
 }
